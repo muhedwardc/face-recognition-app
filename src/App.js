2d20a8524d82ca64679b868e7c167b4a396cf00f
Muhammad Edward Chakim <muhedwardc@gmail.com>
2018-02-10 16:25:51 +0700

    adding react particles js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 1
+      }
+    },
+    number: {
+      value: 104,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    move: {
+      enable: true,
+      direction: "none",
+      out_mode: "out",
+      speed: 10
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles 
+          className='particles'
+          params={particlesOptions}
+        />
         <Navigation />
         <Logo />
         <Rank />
