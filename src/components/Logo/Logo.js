2fa4913b6838c9971d0ad4c9e69ae191163cdf3f
Muhammad Edward Chakim <muhedwardc@gmail.com>
2018-02-10 12:05:29 +0700

    adding logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import faceId from './faceId.png';
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3">
+                    <img clasName="Logo" alt='logo' src={faceId}/>
+                </div>
+            </Tilt>
+        </div>
+    )
+}   
+
+export default Logo;
